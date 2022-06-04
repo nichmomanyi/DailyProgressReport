4e5d032b2022d940cac1f1da77eeba6fd1aef79a
Ashwin <ashwin3082002@gmail.com>
2022-06-04 22:25:16 +0530

    Milestone issue

--- gcrDailyReport.py
+++ gcrDailyReport.py
@@ -161,7 +161,7 @@ if (sidebarContent == "Progress Report"):
             quest, skillbg, per = milestoneCal(10, 5, tindex)
             #per = int(((quest+skillbg)/12)*100)
             st.subheader("Milestone1 :    " + str(per) +"% Completed\n Quests: " + str(quest)+ "/10, Skill Badge: " + str(skillbg)+ "/5")
-            if(quest >= 8 and skillbg >= 4):
+            if(quest >= 10 and skillbg >= 5):
                 st.write("🥳 Congratulations! You have completed your 1st Milestone 🎊🎊🎊")
             else:
                 st.progress(per)
@@ -169,7 +169,7 @@ if (sidebarContent == "Progress Report"):
             #Milestone2
             quest, skillbg, per = milestoneCal(20, 10, tindex)
             st.subheader("Milestone2 :    " + str(per) +"% Completed\n Quests: " + str(quest) + "/20, Skill Badge: " + str(skillbg) + "/10")
-            if (quest >= 16 and skillbg >= 8):
+            if (quest >= 20 and skillbg >= 10):
                 st.write("🥳 Congratulations! You have completed your 2nd Milestone 🎊🎊🎊")
             else:
                 st.progress(per)
@@ -177,7 +177,7 @@ if (sidebarContent == "Progress Report"):
             # Milestone3
             quest, skillbg, per = milestoneCal(30, 15, tindex)
             st.subheader("Milestone3 :    " + str(per) +"% Completed\n Quests: " + str(quest) + "/30, Skill Badge: " + str(skillbg) + "/15")
-            if (quest == 24 and skillbg == 12):
+            if (quest == 30 and skillbg == 15):
                 st.write("🥳 Congratulations! You have completed your 3rd Milestone 🎊🎊🎊")
             else:
                 st.progress(per)
@@ -185,7 +185,7 @@ if (sidebarContent == "Progress Report"):
             # Ultimate Milestone
             quest, skillbg, per = milestoneCal(40, 20, tindex)
             st.subheader("Ultimate Milestone :    " + str(per) +"% Completed\n Quests: " + str(quest) + "/40, Skill Badge: " + str(skillbg) + "/20")
-            if (quest >= 30 and skillbg >= 15):
+            if (quest >= 40 and skillbg >= 20):
                 st.write("🥳 Congratulations! You have completed you Ultimate Milestone 🎊🎊🎊")
             else:
                 st.progress(per)
